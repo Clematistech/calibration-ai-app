070ffd60aa80ac5cee165239b9d9d535042c96e1
Clematistech <62595602+Clematistech@users.noreply.github.com>
2025-07-19 16:52:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Calibration Predictor")
+
+st.title("🔧 AI-Powered Calibration Predictor")
+st.write("This app will let you enter raw sensor data and predict the optimal calibration factor based on historical data.")
